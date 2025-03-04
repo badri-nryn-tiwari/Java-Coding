9d43ff29a2d4895fbc97445c4ac1b80db816724b
badri <badrinarayantiwari01@gmail.com>
2025-03-04 22:34:25 +0530

    abc

---
+++ FibonacciRecursion.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FibonacciRecursion {
+    // Recursive method to find the nth Fibonacci number
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        System.out.println("Fibonacci Series:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+}
